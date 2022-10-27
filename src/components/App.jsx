782636aab0af944e9da5d3c0ab1806a7f2578e3c
makslusak <106471649+makslusak@users.noreply.github.com>
2022-10-27 17:56:10 +0300

    Update class component on react hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,49 @@
-import React, { Fragment, Component } from 'react';
+import { useState } from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  countPositive = () => {
-    let positivePerc = Math.round(
-      (this.state.good * 100) /
-        (this.state.bad + this.state.neutral + this.state.good)
-    );
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-    return isNaN(positivePerc) ? '0%' : `${positivePerc}%`;
+  const feedback = {
+    good: setGood,
+    neutral: setNeutral,
+    bad: setBad,
   };
-  countTotal = () => {
-    let total = this.state.bad + this.state.neutral + this.state.good;
+
+  const handleIncrement = evt => {
+    const { name } = evt.target;
+    feedback[name](prev => prev + 1);
+    console.log(feedback[name]);
+  };
+
+  const countTotal = () => {
+    let total = bad + neutral + good;
     return total;
   };
-  handleIncrement = evt => {
-    const { name } = evt.target;
-    this.setState(prevState => {
-      return {
-        [name]: prevState[name] + 1,
-      };
-    });
+
+  const countPositive = () => {
+    let positivePerc = Math.round((good * 100) / (bad + neutral + good));
+    return isNaN(positivePerc) ? '0%' : `${positivePerc}%`;
   };
-  render() {
-    return (
-      <>
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions handleIncrement={this.handleIncrement} />
-        </Section>
-        <Section title={'Statistics'}>
-          <Statistics
-            countTotal={this.countTotal}
-            countPositive={this.countPositive}
-            state={this.state}
-          />
-        </Section>
-      </>
-    );
-  }
-}
+
+  return (
+    <>
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions handleIncrement={handleIncrement} />
+      </Section>
+      <Section title={'Statistics'}>
+        <Statistics
+          countTotal={countTotal}
+          countPositive={countPositive}
+          good={good}
+          neutral={neutral}
+          bad={bad}
+        />
+      </Section>
+    </>
+  );
+};
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 import { Notification } from './Notification/Notification';
-import { feedbacksArr } from 'feedbacks';
+import { feedbacksArr } from '../../feedbacks';
 
-export const Statistics = ({ countPositive, countTotal, state }) => {
+export const Statistics = ({
+  countPositive,
+  countTotal,
+  good,
+  neutral,
+  bad,
+}) => {
+  const feedbacks = [good, neutral, bad];
+
   return countTotal() === 0 ? (
     <Notification text={'There is no feedback yet'} />
   ) : (
     <div className={css.statsContainer}>
-      {feedbacksArr.map(feedback => (
+      {feedbacksArr.map((feedback, index) => (
         <p key={feedback.title}>
           {feedback.title}
-          <span className={css.statsValue}> : {state[feedback.name]}</span>
+          <span className={css.statsValue}> : {feedbacks[index]}</span>
         </p>
       ))}
       <p>
@@ -28,9 +36,7 @@ export const Statistics = ({ countPositive, countTotal, state }) => {
 Statistics.propTypes = {
   countPositive: PropTypes.func.isRequired,
   countTotal: PropTypes.func.isRequired,
-  state: PropTypes.shape({
-    good: PropTypes.number.isRequired,
-    bad: PropTypes.number.isRequired,
-    neutral: PropTypes.number.isRequired,
-  }),
+  good: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
 };
